b7199160a56bb8307f451434c682f48289c31f9a
Mansi <desaimansi0711@gmail.com>
2024-06-19 16:16:52 +0530

    This is commit from Asiantimezone guy

--- TestNGTutorials/src/test/Day1.java
+++ TestNGTutorials/src/test/Day1.java
@@ -8,7 +8,10 @@ public class Day1 {
 	@AfterTest
 	public void ExecuteLast()
 	{
-	  System.out.println("I will execute Last");	
+	  System.out.println("I will execute Last");
+	  System.out.println("I will execute First");
+	  System.out.println("I will execute Second");
+	  System.out.println("I will execute Third");
 	}
 	
 	
